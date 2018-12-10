a40fcd6a6855ca1416a4bc94e218acd49b7e646e
Manbearpixel <manbearpixel@gmail.com>
2018-12-10 09:54:16 -0600

    Added support for Obsidian () coin

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -2442,3 +2442,28 @@ class BitcoinPlus(Coin):
         '''Given a header return the hash.'''
         import x13_hash
         return x13_hash.getPoWHash(header)
+
+# source: https://github.com/obsidianplatform
+class Obsidian(Coin):
+    NAME = "Obsidian"
+    SHORTNAME = "ODN"
+    NET = "mainnet"
+    XPUB_VERBYTES = bytes.fromhex("0488c21e")
+    XPRV_VERBYTES = bytes.fromhex("0488b2dd")
+    P2PKH_VERBYTE = bytes.fromhex("4b")
+    P2SH_VERBYTES = [bytes.fromhex("7d")]
+    WIF_BYTE = bytes.fromhex("cb")
+    GENESIS_HASH = ('0000006dd8a92f58e952fa61c9402b74'
+                    'a381a69d1930fb5cc12c73273fab5f0a')
+    RPC_PORT = 56661
+    TX_COUNT = 1067887
+    TX_PER_BLOCK = 2
+    TX_COUNT_HEIGHT = 500000
+    DAEMON = daemon.LegacyRPCDaemon
+
+    @classmethod
+    def header_hash(cls, header):
+        '''Given a header return the hash.'''
+        from hashlib import sha512
+        return sha512(header).digest()[:32]
+
